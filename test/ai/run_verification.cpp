b657f63343d68bd12d56fc002e2da8b1d291cb82
Sankha Das <sankha@sankha-cuda.lxd>
2026-01-06 05:14:21 +0000

    cifar_conv_small works

--- test/ai/run_verification.cpp
+++ test/ai/run_verification.cpp
@@ -142,7 +142,7 @@ void field_verification(BoolIO<NetIO> *ios[threads], int* layer_specs, int num_l
         if(party == ALICE)   cout << (verified ? "YES" : "NO") << "\n";
       }
 
-      model_field->describe(false, true);
+      // model_field->describe(false, true);
     }
   } else {
     for(int i = base_example; i < base_example + num_examples; i++){
